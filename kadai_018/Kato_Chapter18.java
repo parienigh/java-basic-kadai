f05e6e1ff0c08d911d840d4313023872fd5288ac
parienigh <8suta.to10621@gmail.com>
2024-07-23 20:32:31 +0900

    kadai_018 をアップロード

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+	  public String familyName = "加藤"; //姓を表す
+	  public String givenName; //名を表す
+	  public String address = "東京都中野区〇×"; //住所を表す
+
+	  public void commonIntroduce() {
+	    System.out.println("名前は" + this.familyName + this.givenName + "です");
+	    System.out.println("住所は" + this.address + "です");
+	  } //共通の紹介を出力する
+
+	  abstract public void eachIntroduce(); //個別の紹介を出力する
+
+	  public void execIutroduce() {
+	    commonIntroduce();
+	    eachIntroduce();
+	  } //紹介を実行する
+}
+
